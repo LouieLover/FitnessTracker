649ef1480d3187d1048b8bcbc7732da1ed150a78
Larry X <papadocarock@gmail.com>
2020-10-21 16:36:32 -0400

    connecting

--- server.js
+++ server.js
@@ -30,11 +30,6 @@ connection.on("error", (err) => {
     console.log("Mongoose connection error: ", err);
 });
 
-// app.get("/", (req, res) => {
-//     res.json({
-//         success: true,
-//     });
-// });
 
 app.get("/api/config", (req, res) => {
     res.json({
@@ -63,60 +58,9 @@ app.get("/workout", (req, res) => {
     });
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("App running on port 3000!");
 });
-// mongodb + srv: //papadocarock@gmail.com:1234@random.a37sp.mongodb.net/fitnesstracker?retryWrites=true&w=majority
 
-
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const app = express();
-
-// const PORT = process.env.PORT || 3000;
-
-// // const ingredientsController = require("./controllers/ingredientsController");
-// // const pizzaController = require("./controllers/pizzaController")
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// mongoose.connect(
-//     process.env.MONGODB_URI || "mongodb://localhost/murmuring-dusk-98089", {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true,
-//         useCreateIndex: true,
-//         useFindAndModify: false,
-//     }
-// );
-
-// const connection = mongoose.connection;
-
-// connection.on("connected", () => {
-//     console.log("Mongoose successfully connected.");
-// });
-
-// connection.on("error", (err) => {
-//     console.log("Mongoose connection error: ", err);
-// });
-
-// app.get("/", (req, res) => {
-//     res.json({
-//         success: true,
-//     });
-// });
-
-// app.get("/api/config", (req, res) => {
-//     res.json({
-//         success: true,
-//     });
-// });
-
-// // app.use(ingredientsController);
-// // app.use(pizzaController);
-
-// app.listen(PORT, () => {
-//     console.log(`Server is running on http://localhost:${PORT}`);
-// });
 
 // mongodb + srv: //1234:4XN0DH2Fjc0kK4YP@random.a37sp.mongodb.net/fitnesstracker?retryWrites=true&w=majority
