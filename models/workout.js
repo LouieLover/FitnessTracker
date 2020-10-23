08ac250859252a42f0ca967ac57d95f4ebf9f550
Larry X <papadocarock@gmail.com>
2020-10-23 08:49:58 -0400

    heroku promblems

--- models/workout.js
+++ models/workout.js
@@ -55,6 +55,6 @@ const WorkoutSchema = new Schema({
 
 });
 
-const Workout = mongoose.model("Workout", WorkoutSchema);
+const Workout = mongoose.model("./Workout", WorkoutSchema);
 
 module.exports = Workout;
